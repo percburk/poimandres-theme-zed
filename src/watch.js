7914e0bf738fd8e232b96e3b6899086c5573c5c3
Kevin Burk <percburk@gmail.com>
2025-11-19 22:28:04 -0600

    functionality is sweet, getting realtime updates in zed, optimized the write process post-prettier

--- src/watch.js
+++ src/watch.js
@@ -2,43 +2,53 @@ import fs from 'node:fs'
 import sharp from 'sharp'
 import chokidar from 'chokidar'
 import prettier from 'prettier'
+import os from 'node:os'
 
 let reloadCount = 0
 const ARROW = '\x1b[32m→\x1b[0m'
+
+const args = process.argv.slice(2)
 
 async function generateTheme() {
   const { base, noitalics, storm, stormNoitalics, createSchema, createSvg } =
     await import(`./theme.js?v=${reloadCount}`)
   reloadCount++
 
+  const schema = createSchema(
+    {
+      theme: base,
+      themeName: 'poimandres-zed',
+    },
+    {
+      theme: storm,
+      themeName: 'poimandres-zed-storm',
+    },
+    {
+      theme: noitalics,
+      themeName: 'poimandres-zed-noitalics',
+    },
+    {
+      theme: stormNoitalics,
+      themeName: 'poimandres-zed-noitalics-storm',
+    }
+  )
+
+  const formatted = await prettier.format(schema, { parser: 'json' })
+
   fs.writeFileSync(
     'themes/poimandres-zed.json',
-    createSchema(
-      {
-        theme: base,
-        themeName: 'poimandres-zed',
-      },
-      {
-        theme: storm,
-        themeName: 'poimandres-zed-storm',
-      },
-      {
-        theme: noitalics,
-        themeName: 'poimandres-zed-noitalics',
-      },
-      {
-        theme: stormNoitalics,
-        themeName: 'poimandres-zed-noitalics-storm',
-      }
-    ),
+    formatted,
     (err) => err && console.log(err)
   )
 
-  const formatted = await prettier.format(
-    fs.readFileSync('themes/poimandres-zed.json', 'utf-8'),
-    { parser: 'json' }
-  )
-  fs.writeFileSync('themes/poimandres-zed.json', formatted)
+  if (args.includes('--sync')) {
+    console.log(`${ARROW} syncing to Zed themes`)
+    fs.writeFileSync(
+      `${os.homedir()}/.config/zed/themes/poimandres-zed.json`,
+      formatted,
+      (err) => err && console.log(err)
+    )
+  }
 
   delete base.colors.black
   delete base.colors.transparent
